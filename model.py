898890260734a52d037444adbfaae694cb5a2b11
kharu_2.17 <kharu217.dev@gmail.com>
2025-05-08 16:06:22 +0900

    commit

--- model.py
+++ model.py
@@ -60,3 +60,108 @@ class PositionalEncoding(nn.Module) :
         x = x + self.pe
 
         return x
+    
+class AttentionHead(nn.Module) :
+    def __init__(self, d_model, head_size):
+        super().__init__()
+        self.head_size = head_size
+
+        self.query = nn.Linear(d_model, head_size)
+        self.key = nn.Linear(d_model, head_size)
+        self.value = nn.Linear(d_model, head_size)
+
+    def forward(self, x) :
+        Q = self.query(x)
+        K = self.key(x)
+        V = self.value(x)
+
+        #Dot Product of Queries and keys
+        attention = Q @ K.transpose(-2, -1)
+
+        #Scaling 
+        attention = attention / (self.head_size ** 0.5)
+        attention = torch.softmax(attention, dim=-1)
+        attention = attention @ V
+
+        return attention
+    
+class MultiHeadAttention(nn.Module) :
+    def __init__(self, d_model, n_heads):
+        super().__init__()
+        self.head_size = d_model // n_heads
+
+        self.w_o = nn.Linear(d_model, d_model)
+
+        self.heads = nn.ModuleList([AttentionHead(d_model, self.head_size) for _ in range(n_heads)])
+    def forward(self, x) :
+        # Combine attention heads
+        out = torch.cat([head(x) for head in self.heads], dim=-1)
+
+        out = self.w_o(out)
+
+        return out
+
+class TransformerEncoder(nn.Module) :
+    def __init__(self, d_model ,n_heads, r_mlp=4):
+        super().__init__()
+        self.d_model = d_model
+        self.n_heads = n_heads
+
+        #Sub-Layer 1 Normalization
+        self.ln1 = nn.LayerNorm(d_model)
+
+        #Multi-Head Attention
+        self.mha - MultiHeadAttention(d_model, n_heads)
+
+        #Sub-Layer 2 Normalization
+        self.ln2 = nn.LayerNorm(d_model)
+
+        #Multilayer Perception
+        self.mlp = nn.Sequential(
+            nn.Linear(d_model, d_model*r_mlp),
+            nn.GELU(),
+            nn.Linear(d_model*r_mlp, d_model)
+        )
+
+    def forward(self, x) :
+        #Residual Connection After Sub-Layer 1
+        out = x + self.mha(self.ln1(x))
+        
+        #Residual Connection After Sub-Layer 2
+        out = out + self.mha(self.ln2(out))
+
+        return out
+    
+class VisionTransformer(nn.Module) :
+    def __init__(self, d_model, n_classes, img_size, patch_size, n_channels, n_heads, n_layers):
+        super().__init__()
+
+        assert img_size[0] % patch_size[0] == 0 and img_size[1] % patch_size[1] == 0, "img_size dimensions must be divisible by patch_size dimesions"
+        assert d_model % n_heads == 0, "d_model must be divisible by n_heads"
+
+        self.d_model = d_model # Dimensionality of Model
+        self.n_classes = n_classes # Number of classes 
+        self.img_size = img_size # Image size
+        self.patch_size = patch_size # Patch size
+        self.n_channels = n_channels # Number of channels
+        self.n_heads = n_heads # Number of attention heads
+
+        self.n_patches = (self.img_size[0] * self.img_size[1]) // (self.patch_size[0] * self.patch_size[1])
+        self.max_seq_length = self.n_patches + 1
+
+        self.patch_embedding = PatchEmbedding(self.d_model, self.img_size, self.patch_size, self.n_channels)
+        self.positional_encoding = PositionalEncoding(self.d_model, self.max_seq_length)
+        self.transformer_encoder = nn.Sequential(*[TransformerEncoder(self.d_model, self.n_heads)])
+>git
+        # Classifier MLP
+        self.classifier = nn.Sequential(
+            nn.Linear(self.d_model, self.n_classes),
+            nn.Softmax(dim=-1)
+        )
+
+    def forward(self, images) :
+        x = self.patch_embedding(images)
+        x = self.positional_encoding(x)
+        x = self.transformer_encoder(x)
+        x = self.classifier(x[:, 0])
+        return x
--- utils.py
+++ utils.py
@@ -16,3 +16,14 @@ image2tensor = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize([0.5, ], [0.5, ])
 ])
+
+d_model = 9
+n_classes = 7
+img_size = (48, 48)
+patch_size = (16, 16)
+n_channels = 1
+n_heads = 3
+n_layers = 3
+batch_size = 128
+epochs = 5
+alpha = 0.005
